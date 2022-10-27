e231408c62bc1e6ccbe42e0d352b3140fefa5baa
mroenca40 <mproenca.marketing@gmail.com>
2022-10-27 09:32:17 -0300

    troca para static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe';
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
 }
 
 // executada no servidor node
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve(
     'price_1LdH5kHT8c6zmgpbeAgQFVYw',
   )
@@ -58,6 +58,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
